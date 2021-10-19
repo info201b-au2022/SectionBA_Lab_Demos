34b79810726b55d240e36912fd095629937dc4ff
Abdiwahid <ahajir@uw.edu>
2021-10-19 10:34:38 -0700

    Updated Demo for 10/19/2021

---
+++ Lab_3_Demos/Lab_3_Demo.R
@@ -0,0 +1,61 @@
+# Question 1. # Load the `stringr` package, which you'll use later.
+
+
+
+# Question 2: Load the data from Starbucks Data 
+# into a variable called `starbucks` rember to store strings AS factors as a false
+
+
+
+
+
+## Question 3: How much  rows are in  the dataset 'num_starbucks'
+
+
+
+
+## Question 4: Extract the `Calories` column into a variable called `num_calories` 
+
+
+
+
+
+
+## Question 5: View your num_calories if you done it correctly above you should only 
+## see the calories column extracted. You should first turn it into a dataframe to view it as nice
+## organized table. 
+
+
+
+
+## Question 6: What is the mean number of Calories? `mean_calories` 
+##(hint for this and other calculations: you'll need to consider missing values)
+
+
+
+
+
+
+
+## Question 7: Write a function `has_more_As` to determine which of two strings contains
+# more instances of the letter "a". It should take as parameters two string
+# variables, and return the argument which has more occurances of the letter "a"
+# If neither phrase contains the letter "a", it should return:
+# "Neither string contains the letter a."
+# If the phrases contain the same number of "a"s, it should return:
+# "The strings have the same number of as."
+# The function must work for both capital and lowercase "a"s.
+
+
+
+
+
+## Question 8: Call your function above and test it by passing it different strings log these out in the terminal
+
+
+
+
+
+## Question 8: Create a vector of the number of fats which are greater than 10.0 in the starbucks dataset
+## `Too_Much_Fat` The Too_Much_Fat should store the number of fats total which are greater than 10.0
+
