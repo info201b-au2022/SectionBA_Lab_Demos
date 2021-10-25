de8aad2739b59158845454f3214a8a3d4288f4ec
iGuest <iguest@IS-M082L23-01.clients.uw.edu>
2021-10-25 16:49:49 -0700

    Demo updated for 10/25

---
+++ Lab_4_Demos/Lab_4_Demo.R
@@ -0,0 +1,59 @@
+## In this demo Your going to be working with the protests Data and answering 
+## Questions asked about the dataset
+
+##=--------Part 1 set up
+##Question 1: Before you begin first load the stringr, dyply for know as a first step.
+## These might come in handy later
+
+
+
+
+
+##Question 2: Next load the dataset from Assignment 1 from https://countlove.org/data/data.csv
+# into a variable called `protests`
+
+
+
+
+##----- Part 2 Answering questions
+
+##Question 3: which Location had the most Attendes by the most recent date. 
+#Rember to filter out na Values `most_attendes`
+
+
+
+
+##Question 4: What was the total number of attendes that attended the protests in Milwaukee, WI. For 
+## Date 10/19/2015. `get_attendes_data`
+
+
+
+
+
+## Challenge Question 5:
+
+## How many total number of attendes were reported there were there in total for all protests.
+## Rember this question is asking for all protests and the sum of all the Attendes that went to this protests 
+## Average
+
+
+
+##Question 6:
+## What was the maxiumn number of attendes ever recored for 2017. Remeber 2017 is the lowest date 
+## and 2021 is the maxiumn most recent date. Store this in a varible in_2017_protests
+
+
+
+
+
+## Challenge bonus: Write a function which takes in  a Tags(The type of protests) as a parameter. 
+## And finds out how much Attendes attended that protests in total. When your function 
+## Is called it should return the phrase "There were N attendees for the protests T"
+## Where N is the total number of attendes calculated and  T is the Tag the parameter passed in. 
+
+
+
+## Challenge bonus continued: Call your function above and pass it the 
+## Tag "Civil Rights; For racial justice; Against punishment" to find out how  much Attendes 
+## attended this protest
+
