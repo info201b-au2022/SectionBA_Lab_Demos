c4ac8e8759ad2b0d85f3b9c58e5a4bcb022fb880
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-01-10 11:42:02 -0800

    Update demo for 1/10

---
+++ Lab_1_Demos/Lab_1_Demo.R
@@ -0,0 +1,29 @@
+# Overview ----------------------------------------------------------------
+# For grading, it's important that you store your answers in the variable names
+# listed with each question in `backtics`.
+# For each prompt marked `Reflection`, please write a response
+# in your `README.md` file.
+
+## Create a variable `sum` which is the sum of 10 and 20
+
+
+
+
+## Create a variable `mood` which describes your mood
+
+
+
+
+## Create a variable `date` which is Today's Date
+
+
+
+##What is the max of these numbers 10, 40, 50, 100, 400, 135, 70 `max_total`
+
+
+
+
+
+
+
+# Reflection: Create a header with your name.
